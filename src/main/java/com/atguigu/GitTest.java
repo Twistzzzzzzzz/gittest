59f22664dd3b98f710d86a97a4faae5dd5e9d4c7
王渤 <1362268562@qq.com>
2021-01-18 16:16:03 +0800

    更新4

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("123456");
         System.out.println("222222");
+        System.out.println("444444");
     }
 }
